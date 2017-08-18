b8b41e7c1ec032611035d15b4ab21d21d15e6f29
rui4u <sr_sharui@163.com>
2017-08-18 19:35:30 +0800

    计算价格模型

--- AppBaseDemo/AppBaseClass/ShoppingCart/Model/CountPriceModel.h
+++ AppBaseDemo/AppBaseClass/ShoppingCart/Model/CountPriceModel.h
@@ -9,5 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface CountPriceModel : NSObject
-
+@property (nonatomic , copy) NSString              * totalcashPeldge;
+@property (nonatomic , copy) NSString              * carNum;
+@property (nonatomic , copy) NSString              * isSucceed;
+@property (nonatomic , copy) NSString              * totalcurrentPrice;
+@property (nonatomic , copy) NSString              * totalPrice;
 @end
