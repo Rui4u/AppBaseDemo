83143fc697a88329d0c295e37cb4b96f521d641c
sharui <sr_sharui@qq.com>
2017-08-19 17:45:18 +0800

    添加快速反馈

---
+++ AppBaseDemo/AppBaseClass/Mine/View/NewDemandView.h
@@ -0,0 +1,13 @@
+//
+//  NewDemandView.h
+//  AppBaseDemo
+//
+//  Created by sharui on 2017/8/19.
+//  Copyright © 2017年 sharui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewDemandView : UIView
+
+@end
