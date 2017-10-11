c46ffecd239fd80ad6e0be4cfb56fb0b81f9e54e
rui4u <sr_sharui@163.com>
2017-10-11 14:18:07 +0800

    提交代码

--- AppBaseDemo/AppBaseClass/Mine/ViewController/MoreViewController.m
+++ AppBaseDemo/AppBaseClass/Mine/ViewController/MoreViewController.m
@@ -98,7 +98,7 @@
 		NSString *path = [[NSBundle mainBundle] pathForResource:@"protocolText" ofType:@"text"];
 		NSString *content = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
 		
-		NSString * string = [NSString stringWithFormat:@"商户信息\n\n商户ID:%@\n\n门店名称:%@\n\n负责人:%@\n\n负责人电话:%@\n\n%@",
+		NSString * string = [NSString stringWithFormat:@"商户信息\n\n商户ID:%@\n\n门店名称:%@\n\n负责人:%@\n\n负责人电话:%@\n\n协议内容\n\n%@",
 							 (NSString *)[[NSUserDefaults standardUserDefaults] objectForKey:@"id"],
 							 (NSString *)[[NSUserDefaults standardUserDefaults] objectForKey:@"storeName"],
 							  (NSString *)[[NSUserDefaults standardUserDefaults] objectForKey:@"managerName"],
